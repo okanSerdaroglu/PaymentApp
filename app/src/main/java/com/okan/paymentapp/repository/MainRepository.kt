48698693d0cab1eb03c7645630a63e0160c341cc
Okan Serdaroğlu <okanserdaroglu2@gmail.com>
2020-09-27 00:41:41 +0300

    MainRepository With Send Payment Method

---
+++ app/src/main/java/com/okan/paymentapp/repository/MainRepository.kt
@@ -0,0 +1,28 @@
+package com.okan.paymentapp.repository
+
+import com.okan.paymentapp.api.NetworkMapper
+import com.okan.paymentapp.api.PaymentAPI
+import com.okan.paymentapp.model.PaymentResult
+import com.okan.paymentapp.util.DataState
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import javax.inject.Inject
+
+class MainRepository
+@Inject
+constructor(
+    private val paymentAPI: PaymentAPI,
+    private val networkMapper: NetworkMapper
+) {
+    suspend fun sendPayment(): Flow<DataState<PaymentResult>> = flow {
+        emit(DataState.Loading)
+        delay(1000) // delay is just seeing the progress
+        try {
+            val paymentResult = paymentAPI.sendPaymentRequest()
+            emit(DataState.Success(networkMapper.mapFromEntity(paymentResult)))
+        } catch (e: Exception) {
+            emit(DataState.Error(e))
+        }
+    }
+}
